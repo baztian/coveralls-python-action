5a1ee043b798fda2874ea789e88f5fd1e2c9b8b2
baztian <bastian.dev@gmail.com>
2020-06-19 23:58:46 +0200

    Allow for specifiying which coverage version to use

---
+++ src/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+COVERAGE_VERSION=""
+#!/bin/bash
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --coverage-version)
+    COVERAGE_VERSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ "$COVERAGE_VERSION" != "" ]; then
+    pip install coverage==$COVERAGE_VERSION
+fi
+pip install coveralls
+./src/entrypoint.py $@
