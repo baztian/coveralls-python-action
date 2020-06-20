99ee625c419cbca935ee61d4695858ec5a15a874
baztian <bastian.dev@gmail.com>
2020-06-21 00:25:04 +0200

    Fix repo name substitution

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -25,8 +25,8 @@ if [ "$COVERAGE_VERSION" != "" ]; then
     pip install --upgrade coverage==$COVERAGE_VERSION
     if [ "${COVERAGE_VERSION:0:1}" -lt "5" ]; then
         ( set -o posix ; set )
-        echo "foo${RUNNER_WORKSPACE}bar"
-        sed -i "s#$RUNNER_WORKSPACE/##g" .coverage
+        echo "foo${RUNNER_WORKSPACE}/${GITHUB_REPOSITORY#*/}/bar"
+        sed -i "s,$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/,,g" .coverage
         cat .coverage
     fi
 fi
