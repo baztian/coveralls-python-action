bdef92dd6ae16cf21e4fae6803f414538d9ad661
baztian <bastian.dev@gmail.com>
2020-06-21 22:18:20 +0200

    Remove debug code

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -24,11 +24,7 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 if [ "$COVERAGE_VERSION" != "" ]; then
     pip install --upgrade coverage==$COVERAGE_VERSION
     if [ "${COVERAGE_VERSION:0:1}" -lt "5" ]; then
-        ( set -o posix ; set )
-        echo "foo${RUNNER_WORKSPACE}/${RUNNER_WORKSPACE##*/}/bar"
         sed -i "s,$RUNNER_WORKSPACE/${RUNNER_WORKSPACE##*/},$GITHUB_WORKSPACE,g" .coverage
-        ls -la
-        cat .coverage
     fi
 fi
 /src/entrypoint.py $@
